66ca6ef7d67c81e519bf85a53639da94b7745d0e
EIA485 <17285570+EIA485@users.noreply.github.com>
2021-10-04 03:36:04 -0700

    add credit and make variable name generic

--- CollectionsWorkshop/SyncLinearKey.cs
+++ CollectionsWorkshop/SyncLinearKey.cs
@@ -6,9 +6,9 @@ namespace CollectionsWorkshop
 	public class SyncLinearKey<T> : SyncObject where T : IEquatable<T>
 	{
 		[Range(0.0f, 1f, "0.000")]
-		public readonly Sync<float> GradientPoint;
+		public readonly Sync<float> Point;
 		[Range(0.0f, 1f, "0.00")]
 		public readonly Sync<T> value;
-		public static implicit operator LinearKey<T>(SyncLinearKey<T> key) => new LinearKey<T>(key.GradientPoint.Value, key.value.Value);
+		public static implicit operator LinearKey<T>(SyncLinearKey<T> key) => new LinearKey<T>(key.Point.Value, key.value.Value);
 	}
 }
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CollectionsWorkshop
 A simple plugin that allows you to edit SyncLinear Fields.
 
+based on Gareth's [ParticleWorkshop](https://github.com/New-Project-Final-Final-WIP/ParticleWorkshop)
+
 ## Installation
 1. download [CollectionsWorkshop.dll](https://github.com/eia485/CollectionsWorkshop/releases/latest/download/CollectionsWorkshop.dll)
 2. place CollectionsWorkshop.dll into C:\Program Files (x86)\Steam\steamapps\common\NeosVR\Libraries
